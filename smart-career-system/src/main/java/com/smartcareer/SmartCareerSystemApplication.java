0e64cc44aa16146d65fd9d71bcba404fad66dd67
Emanuel002-code <tumelomafalo77@gmail.com>
2026-07-11 23:01:21 +0200

    Project setup

---
+++ smart-career-system/src/main/java/com/smartcareer/SmartCareerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.smartcareer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCareerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCareerSystemApplication.class, args);
+	}
+
+}
